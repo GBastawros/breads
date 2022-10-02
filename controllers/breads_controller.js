3d42b8deea3c924a37d0e75a48f8b2c5ae894958
George Bastawros <george.bastawros@outlook.com>
2022-10-02 11:20:48 -0500

    delete route complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -4,12 +4,12 @@ const Bread = require('../models/bread.js')
 
 // INDEX
 breads.get('/', (req, res) => {
-    res.render('index',
-      {
-        breads: Bread,
-        title: 'index Page'
-      }
-    )
+  res.render('index',
+    {
+      breads: Bread,
+      title: 'index Page'
+    }
+  )
   // res.send(Bread)
 })
 
@@ -17,7 +17,8 @@ breads.get('/', (req, res) => {
 breads.get('/:arrayIndex', (req, res) => {
   if (Bread[req.params.arrayIndex]) {
     res.render('Show', {
-      bread:Bread[req.params.arrayIndex]
+      bread: Bread[req.params.arrayIndex],
+      index: req.params.arrayIndex,
     })
   } else {
     res.send('404')
@@ -29,7 +30,7 @@ breads.post('/', (req, res) => {
   if (!req.body.image) {
     req.body.image = 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
   }
-  if(req.body.hasGluten === 'on') {
+  if (req.body.hasGluten === 'on') {
     req.body.hasGluten = true
   } else {
     req.body.hasGluten = false
@@ -42,6 +43,12 @@ breads.post('/', (req, res) => {
 // NEW
 breads.get('/new', (req, res) => {
   res.render('new')
+})
+
+// DELETE
+breads.delete('/:indexArray', (req, res) => {
+  Bread.splice(req.params.indexArray, 1)
+  res.status(303).redirect('/breads')
 })
 
 module.exports = breads
--- views/show.jsx
+++ views/show.jsx
@@ -1,26 +1,31 @@
 const React = require('react')
 const Default = require('./layouts/Default')
 
-function show ({bread}) {
-    // Confim we arfe getting our bread data in the terminal.
-    // console.log(bread.name)
-    return (
-        <Default>
-        <h3>{bread.name}</h3>
-        <p>
-          and it
-          {
-            bread.hasGluten
+function show({bread, index}) {
+  // Confim we arfe getting our bread data in the terminal.
+  // console.log(bread.name)
+  return (
+    <Default>
+      <h3>{bread.name}</h3>
+      <p>
+        and it
+        {
+          bread.hasGluten
             ? <span> does </span>
             : <span> does NOT </span>
-          }
-          have gluten.
-        </p>
-        <img src={bread.image} alt={bread.name} />
-        <li><a href="/breads">Go home</a></li>
-      </Default>
-      
-    )
+        }
+        have gluten.
+      </p>
+      <img src={bread.image} alt={bread.name} />
+      <li><a href="/breads">Go home</a></li>
+
+      <form action={`/breads/${index}?_method=DELETE`} method="POST">
+        <input type='submit' value="DELETE" />
+      </form>
+
+    </Default>
+
+  )
 }
 
 module.exports = show
